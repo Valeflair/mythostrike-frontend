354de8b199f287bc87e98273af5f3bc791235cd1
Hong Zhang <ukybo@student.kit.edu>
2023-01-22 22:28:25 +0100

    Applied code formatter and removed whitespaces

--- src/views/LobbyOverview.vue
+++ src/views/LobbyOverview.vue
@@ -1,3 +1,1 @@
-<template>
-    lobby
-</template>
+<template>lobby</template>
--- src/views/Start.vue
+++ src/views/Start.vue
@@ -1,48 +1,60 @@
 <template>
-    <div class="bg-image">
-    <v-row class="pt-16 text-center">
-      <v-col cols="1"></v-col>
-      <v-col cols="4" class="text-right">
+  <div class="bg-image">
+    <div class="d-flex justify-center">
+      <v-col cols="2" class="text-right">
         <img
-        alt="Vue logo"
-        class="logo"
-        src="@/assets/logo.png"
-        width="300"
-        height="300"
-      />
+          alt="Vue logo"
+          class="logo"
+          src="@/assets/logo.png"
+          width="300"
+          height="300"
+        />
       </v-col>
-      <v-col cols="4" class="text-left">
-        <div class="text-h2 text-center pt-8">
+      <v-col cols="4" class="text-center">
+        <div class="text-h2 pt-8">
           <span id="text">Μ ψ τ η ο σ τ ρ ι κ ε</span>
         </div>
         <div class="text-h1 font-weight-regular" id="title">
           <span id="text">ΜYTHOSTRIKE</span>
         </div>
-        <div class="text-h4 text-center pa-2">
+        <div class="text-h4 pa-3">
           <span id="text">-online-</span>
         </div>
       </v-col>
-      <v-col cols="1"></v-col>
-    </v-row>
-</div>
+    </div>
+  </div>
 </template>
 <style scoped>
 .bg-image {
-  background: url('@/assets/backgrounds/start_background.png');
+  background: url("@/assets/backgrounds/start_background.png");
   height: 100vh;
 }
 
 #text {
-  color:white;
+  color: white;
 }
 
-#title{
+#title {
   text-decoration-line: underline;
   text-decoration-color: white;
   text-decoration-thickness: 5px;
 }
 
-.text-h1{
+.text-h1 {
   text-underline-offset: 10px;
 }
+
+
+.text-field{
+  color: white;
+}
 </style>
+<script>
+export default {
+  name: "Start",
+  data: () => ({
+    username: "",
+    password: "",
+  }),
+};
+</script>
