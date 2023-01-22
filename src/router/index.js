7551c9dec03c8b352bab8285e1c8cc1d5eed9d27
Hong Zhang <ukybo@student.kit.edu>
2023-01-22 14:04:29 +0100

    Adjusted the project structure

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,33 @@
 import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: "/",
-      name: "home",
-      component: HomeView,
+      name: "Start",
+      component: () => import("../views/Start.vue")
     },
     {
-      path: "/about",
-      name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import("../views/AboutView.vue"),
+      path: "/home",
+      name: "Home",
+      component: () => import("../views/Home.vue")
     },
+    {
+      path: "/lobby",
+      name: "Lobby",
+      component: () => import("../views/Lobby.vue")
+    },
+    {
+      path: "/lobbyoverview",
+      name: "LobbyOverview",
+      component: () => import("../views/LobbyOverview.vue")
+    },
+    {
+      path: "/game",
+      name: "Game",
+      component: () => import("../views/game.vue")
+    }
   ],
 });
 
