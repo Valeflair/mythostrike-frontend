7bb67ea9e53ded46a3ddab815d67b6b77374a40a
Hong Zhang <ukybo@student.kit.edu>
2023-01-23 04:07:18 +0100

    Updated assets and implemented user register and login. Installed axios for http request

--- src/views/Start.vue
+++ src/views/Start.vue
@@ -11,24 +11,23 @@
         />
       </v-col>
       <v-col cols="4" class="text-center">
-        <div class="text-h2 pt-8">
+        <div class="text-h2 pt-10">
           <span id="text">Μ ψ τ η ο σ τ ρ ι κ ε</span>
         </div>
         <div class="text-h1 font-weight-regular" id="title">
           <span id="text">ΜYTHOSTRIKE</span>
         </div>
-        <div class="text-h4 pa-3">
+        <div class="text-h4 pa-4">
           <span id="text">-online-</span>
         </div>
       </v-col>
     </div>
-    
-      
     <v-responsive class="mx-auto pt-5" max-width="450">
       <v-text-field
         class="text-field"
         label="username"
         variant="outlined"
+        v-model="username"
         clearable
       ></v-text-field>
       <v-text-field
@@ -36,15 +35,29 @@
         label="password"
         variant="outlined"
         type="password"
+        v-model="password"
         clearable
       ></v-text-field>
     </v-responsive>
+    <div class="text-center">
+      <v-btn class="startButton">Register</v-btn>
+      <v-btn class="startButton">Login</v-btn>
+    </div>
   </div>
 </template>
 <style scoped>
 .bg-image {
-  background: url("@/assets/backgrounds/lobby_background.png");
+  background: url("@/assets/backgrounds/start_background.png");
   height: 100vh;
+}
+
+.startButton {
+  width: 227px;
+  min-height: 61px;
+  margin: 15px;
+  color: white;
+  font-size: 20px;
+  background: url("@/assets/elements/start_button.png");
 }
 
 #text {
@@ -58,20 +71,57 @@
 }
 
 .text-h1 {
-  text-underline-offset: 10px;
+  text-underline-offset: 15px;
 }
 
-.text-field{
+.text-field {
   color: white;
+}
+
+.startButton {
+  margin: 15px;
 }
 </style>
 
 <script>
+import axios from "axios";
 export default {
   name: "Start",
   data: () => ({
     username: "",
     password: "",
   }),
+  methods: {
+    async login() {
+      await axios
+        .post("http://localhost:8080/users/login", {
+          username: this.username,
+          password: this.password,
+        })
+        .then(
+          (response) => {
+            console.log(response);
+          },
+          (error) => {
+            console.log(error);
+          }
+        );
+    },
+    async register() {
+      let response = await axios
+        .post("http://localhost:8080/users/register", {
+          username: this.username,
+          password: this.password,
+        })
+        .then(
+          (response) => {
+            console.log(response);
+          },
+          (error) => {
+            console.log(error);
+          }
+        );
+    },
+  },
 };
 </script>
