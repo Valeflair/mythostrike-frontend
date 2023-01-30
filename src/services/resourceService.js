148170b49702c2853e5019e6cc6a2cdc6fdebbf2
Hong Zhang <ukybo@student.kit.edu>
2023-01-30 16:55:27 +0100

    Implemented join lobby and create lobby function

--- src/services/resourceService.js
+++ src/services/resourceService.js
@@ -2,10 +2,5 @@ import axios from "axios";
 import service from "@/services/service.js";
 
 export default {
-  getLobbies() {
-    return axios({
-      method: "GET",
-      url: service.RES_LOBBY_URL,
-    });
-  },
+  
 };
--- src/views/LobbyOverview.vue
+++ src/views/LobbyOverview.vue
@@ -1,26 +1,26 @@
 <template>
   <div class="d-flex justify-center">
     <v-col cols="3" id="left-bar" class="text-center">
+      <img
+              :src="
+                '../src/assets/avatars/avatar' +
+                userStore.getUserData.avatarNumber +
+                '.png'
+              "
+            />
+            <v-divider inset></v-divider>
       <div class="text-h4 pa-4">
+        <div class="pa-5"><v-btn class="button" @click="getLobbies">Refresh Lobby</v-btn></div>
         <span id="text">Join lobby</span>
         <v-text-field
           class="text-field pt-5"
           label="LobbyID"
+          v-model="lobbyId"
           variant="outlined"
           clearable
         ></v-text-field>
-        <v-btn class="lobbyButton">Join</v-btn>
-      </div>
-
-      <div class="text-h4 pt-16">
-        <span id="text">Create lobby</span>
-        <v-text-field
-          class="text-field pt-5"
-          label="LobbyID"
-          variant="outlined"
-          clearable
-        ></v-text-field>
-        <v-btn class="lobbyButton">Create</v-btn>
+        <v-btn class="button" @click="joinLobby">Join</v-btn>
+        <div class="pt-10"><v-btn class="button" @click="createLobby">Create Lobby</v-btn></div>
       </div>
     </v-col>
     <v-col cols="9" id="right-bar">
@@ -43,7 +43,7 @@
               <td>{{ lobby.owner }}</td>
               <td>{{ lobby.mode }}</td>
               <td>{{ lobby.numberOfPlayers }}</td>
-              <td><v-btn variant="outlined">Join</v-btn></td>
+              <td><v-btn variant="outlined" @click="joinLobbyById(lobby.id)">Join</v-btn></td>
             </tr>
           </tbody>
         </v-table>
@@ -78,7 +78,7 @@ tr:hover {
   color: white;
 }
 
-.lobbyButton {
+.button {
   width: 197px;
   min-height: 61px;
   color: white;
@@ -89,14 +89,21 @@ tr:hover {
 </style>
 
 <script>
-import resourceService from "@/services/resourceService";
+import lobbyService from "@/services/lobbyService";
+import { useUserStore } from "@/stores/user";
 export default {
   data: () => ({
     lobbies: [],
+    lobbyId: ""
   }),
+  setup() {
+    const userStore = useUserStore();
+
+    return { userStore };
+  },
   methods: {
     async getLobbies() {
-      await resourceService.getLobbies().then(
+      await lobbyService.getLobbies().then(
         (response) => {
           console.log(response);
           this.lobbies = response.data;
@@ -106,8 +113,40 @@ export default {
         }
       );
     },
+    async joinLobby() {
+      console.log(this.lobbyId);
+      await lobbyService.joinLobby(this.lobbyId).then(
+        (response) => {
+          console.log(response);
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+    },
+    async joinLobbyById(id){
+      console.log(id);
+      await lobbyService.joinLobby(id).then(
+        (response) => {
+          console.log(response);
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+    },
+    async createLobby() {
+      await lobbyService.createLobby().then(
+        (response) => {
+          console.log(response);
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+    }
   },
-  async created() {
+  created() {
     this.getLobbies();
   },
 };
