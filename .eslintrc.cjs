51f305e9cabb78fbb85c2ec967a7ad4aa8e10a9b
Till <Till-Bernas@web.de>
2023-02-14 16:35:30 +0100

    added prettier configuration to make automatic line endings

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,6 +6,7 @@ module.exports = {
   extends: [
     "plugin:vue/vue3-essential",
     "eslint:recommended",
+    "plugin:prettier/recommended",
     "@vue/eslint-config-prettier",
   ],
   parserOptions: {
